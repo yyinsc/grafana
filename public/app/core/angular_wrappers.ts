9c20fa24c22e14dba0ba6b79ca8bf6e12e2d1ede
joshhunt <josh@trtr.co>
2021-02-03 16:20:43 +0000

    replace replaceTemplateVariable with datasource.replace, and rename onQueryChange to onChange

--- public/app/core/angular_wrappers.ts
+++ public/app/core/angular_wrappers.ts
@@ -201,7 +201,6 @@ export function registerAngularDirectives() {
   react2AngularDirective('azureMonitorQueryEditor', QueryEditor, [
     'query',
     ['datasource', { watchDepth: 'reference' }],
-    'replaceTemplateVariable',
-    'onQueryChange',
+    'onChange',
   ]);
 }
